ea2ec56b57c8cde3dfd910e64b08241e16346a83
Abdur Rouf Rasel <84445833+DROBON@users.noreply.github.com>
2021-07-03 01:18:00 +0600

    Delete controllers directory

--- controllers/system_controller.py
+++
@@ -1,9 +0,0 @@
-from os import name
-
-
-class SystemController:
-    def __init__(self, sys_name=name):
-        self._sys_name = sys_name
-
-    def is_unix(self):
-        return True if self._sys_name == 'posix' else False
